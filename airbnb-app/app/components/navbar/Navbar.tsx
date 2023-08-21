ec73373edfc11a2fa5dee60ee697aceb02d4f7c3
William Mao <tomcat_mao@hotmail.com>
2023-08-20 23:07:59 -0600

    Added Search and avatar

--- airbnb-app/app/components/navbar/Navbar.tsx
+++ airbnb-app/app/components/navbar/Navbar.tsx
@@ -1,5 +1,7 @@
 import Container from '../Container';
 import Logo from './Logo';
+import Search from './Search';
+import UserMenu from './UserMenu';
 
 const Navbar = () => {
   return (
@@ -22,6 +24,8 @@ const Navbar = () => {
             "
           >
             <Logo />
+            <Search />
+            <UserMenu />
           </div>
         </Container>
       </div>
---
+++ airbnb-app/app/components/navbar/UserMenu.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { AiOutlineMenu } from 'react-icons/ai';
+import Avatar from '../../components/Avatar';
+
+const UserMenu = () => {
+  return (
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <div
+          onClick={() => {}}
+          className="
+            hidden
+            md:block
+            text-sm
+            py-3
+            px-4
+            rounded-full
+            hover:bg-neutral-100
+            transition
+            cursor-pointer
+          "
+        >
+          Airbnb your home
+        </div>
+        <div
+          onClick={() => {}}
+          className="
+            p-4
+            md:py-1
+            md:px-2
+            border-[1px]
+            border-neutral-200
+            flex
+            felx-row
+            items-center
+            gap-3
+            rounded-full
+            cursor-pointer
+            hover:shadow-md
+            transition
+          "
+        >
+          <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserMenu;
