0caaaa30a48d2d9327d1de41e7baee9c74c7be6c
William Mao <william.mao.ca@gmail.com>
2023-10-28 19:53:43 -0600

    Added reset category function on the Logo.

--- airbnb-app/app/components/navbar/Categories.tsx
+++ airbnb-app/app/components/navbar/Categories.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Container from '@/app/components/Container';
 import { TbBeach, TbMountain, TbPool } from 'react-icons/tb';
 import {
--- airbnb-app/app/components/navbar/Logo.tsx
+++ airbnb-app/app/components/navbar/Logo.tsx
@@ -13,6 +13,7 @@ const Logo = () => {
       height="100"
       width="100"
       src="/images/logo.png"
+      onClick={() => router.push('/')}
     />
   );
 };
