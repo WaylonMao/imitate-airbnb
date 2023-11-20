eee749f555972aa6bb345e6cf367826fd7a00bd0
William Mao <william.mao.ca@gmail.com>
2023-11-20 10:34:04 -0700

    Create Info section and custom Counter.

--- airbnb-app/app/components/modals/RentModal.tsx
+++ airbnb-app/app/components/modals/RentModal.tsx
@@ -8,6 +8,7 @@ import Heading from '@/app/components/Heading';
 import { categories } from '@/app/components/navbar/Categories';
 import CategoryInput from '@/app/components/inputs/CategoryInput';
 import CountrySelect from '@/app/components/inputs/CountrySelect';
+import Counter from '@/app/components/inputs/Counter';
 import dynamic from 'next/dynamic';
 
 enum STEPS {
@@ -45,7 +46,9 @@ const RentModal = () => {
 
   const category = watch('category');
   const location = watch('location');
-
+  const guestCount = watch('guestCount');
+  const roomCount = watch('roomCount');
+  const bathroomCount = watch('bathroomCount');
 
   const Map = useMemo(
     () => dynamic(() => import('@/app/components/Map'), { ssr: false }),
@@ -132,6 +135,42 @@ const RentModal = () => {
     );
   }
 
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Share some basics about your place"
+          subtitle="What amenities do you have?"
+        />
+        <Counter
+          title="Guests"
+          subtitle="How many guests can your place accommodate?"
+          value={guestCount}
+          onChange={(value) => {
+            setCustomValue('guestCount', value);
+          }}
+        />
+        <hr />
+        <Counter
+          title="Rooms"
+          subtitle="How many rooms can guests use?"
+          value={roomCount}
+          onChange={(value) => {
+            setCustomValue('roomCount', value);
+          }}
+        />
+        <hr />
+        <Counter
+          title="Bathrooms"
+          subtitle="How many bathrooms can guests use?"
+          value={bathroomCount}
+          onChange={(value) => {
+            setCustomValue('bathroomCount', value);
+          }}
+        />
+      </div>
+    );
+  }
   return (
     <Modal
       isOpen={rentModal.isOpen}
