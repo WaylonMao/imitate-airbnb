44fa03922821b29c02fea103e41cf9798a79feb1
WaylonMao <william.mao.ca@gmail.com>
2023-11-30 11:17:25 -0700

    Created Properties page.

--- airbnb-app/app/page.tsx
+++ airbnb-app/app/page.tsx
@@ -1,13 +1,17 @@
-import getListings from '@/app/actions/getListings';
 import getCurrentUser from '@/app/actions/getCurrentUser';
 
 import ClientOnly from '@/app/components/ClientOnly';
 import Container from '@/app/components/Container';
 import EmptyState from '@/app/components/EmptyState';
 import ListingCard from '@/app/components/listings/ListingCard';
+import getListings, { IListingParams } from '@/app/actions/getListings';
 
-export default async function Home() {
-  const listings = await getListings();
+interface HomeProps {
+  searchParams: IListingParams;
+}
+
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
 
   if (listings.length === 0)
@@ -46,4 +50,6 @@ export default async function Home() {
       </Container>
     </ClientOnly>
   );
-}
+};
+
+export default Home;
